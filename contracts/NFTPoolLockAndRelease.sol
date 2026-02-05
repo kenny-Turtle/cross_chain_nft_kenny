5d7bf09b19092d7b376f9184ff5d361df25d4819
zfj <fengj.zhang@sunyard.com>
2026-02-05 08:38:59 +0800

    添加pool文件（删除无用代码、安装依赖、调整版本）

--- contracts/NFTPoolLockAndRelease.sol
+++ contracts/NFTPoolLockAndRelease.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 import {CCIPReceiver} from "@chainlink/contracts-ccip/contracts/applications/CCIPReceiver.sol";
 import {IRouterClient} from "@chainlink/contracts-ccip/contracts/interfaces/IRouterClient.sol";
 import {Client} from "@chainlink/contracts-ccip/contracts/libraries/Client.sol";
-import {OwnerIsCreator} from "@chainlink/contracts@1.4.0/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {OwnerIsCreator} from "@chainlink/contracts/src/v0.8/shared/access/OwnerIsCreator.sol";
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -60,14 +60,6 @@ contract Messenger is CCIPReceiver, OwnerIsCreator {
   bytes32 private s_lastReceivedMessageId; // Store the last received messageId.
   string private s_lastReceivedText; // Store the last received text.
 
-  // Mapping to keep track of allowlisted destination chains.
-  mapping(uint64 => bool) public allowlistedDestinationChains;
-
-  // Mapping to keep track of allowlisted source chains.
-  mapping(uint64 => bool) public allowlistedSourceChains;
-
-  // Mapping to keep track of allowlisted senders.
-  mapping(address => bool) public allowlistedSenders;
 
   IERC20 private s_linkToken;
 
@@ -81,31 +73,7 @@ contract Messenger is CCIPReceiver, OwnerIsCreator {
     s_linkToken = IERC20(_link);
   }
 
-  /// @dev Modifier that checks if the chain with the given destinationChainSelector is allowlisted.
-  /// @param _destinationChainSelector The selector of the destination chain.
-  modifier onlyAllowlistedDestinationChain(
-    uint64 _destinationChainSelector
-  ) {
-    if (!allowlistedDestinationChains[_destinationChainSelector]) {
-      revert DestinationChainNotAllowlisted(_destinationChainSelector);
-    }
-    _;
-  }
 
-  /// @dev Modifier that checks if the chain with the given sourceChainSelector is allowlisted and if the sender is
-  /// allowlisted.
-  /// @param _sourceChainSelector The selector of the destination chain.
-  /// @param _sender The address of the sender.
-  modifier onlyAllowlisted(
-    uint64 _sourceChainSelector,
-    address _sender
-  ) {
-    if (!allowlistedSourceChains[_sourceChainSelector]) {
-      revert SourceChainNotAllowlisted(_sourceChainSelector);
-    }
-    if (!allowlistedSenders[_sender]) revert SenderNotAllowlisted(_sender);
-    _;
-  }
 
   /// @dev Modifier that checks the receiver address is not 0.
   /// @param _receiver The receiver address.
@@ -116,29 +84,7 @@ contract Messenger is CCIPReceiver, OwnerIsCreator {
     _;
   }
 
-  /// @dev Updates the allowlist status of a destination chain for transactions.
-  function allowlistDestinationChain(
-    uint64 _destinationChainSelector,
-    bool allowed
-  ) external onlyOwner {
-    allowlistedDestinationChains[_destinationChainSelector] = allowed;
-  }
 
-  /// @dev Updates the allowlist status of a source chain for transactions.
-  function allowlistSourceChain(
-    uint64 _sourceChainSelector,
-    bool allowed
-  ) external onlyOwner {
-    allowlistedSourceChains[_sourceChainSelector] = allowed;
-  }
-
-  /// @dev Updates the allowlist status of a sender for transactions.
-  function allowlistSender(
-    address _sender,
-    bool allowed
-  ) external onlyOwner {
-    allowlistedSenders[_sender] = allowed;
-  }
 
   /// @notice Sends data to receiver on the destination chain.
   /// @notice Pay for fees in LINK.
@@ -153,9 +99,6 @@ contract Messenger is CCIPReceiver, OwnerIsCreator {
     string calldata _text
   )
     external
-    onlyOwner
-    onlyAllowlistedDestinationChain(_destinationChainSelector)
-    validateReceiver(_receiver)
     returns (bytes32 messageId)
   {
     // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
@@ -184,55 +127,12 @@ contract Messenger is CCIPReceiver, OwnerIsCreator {
     return messageId;
   }
 
-  /// @notice Sends data to receiver on the destination chain.
-  /// @notice Pay for fees in native gas.
-  /// @dev Assumes your contract has sufficient native gas tokens.
-  /// @param _destinationChainSelector The identifier (aka selector) for the destination blockchain.
-  /// @param _receiver The address of the recipient on the destination blockchain.
-  /// @param _text The text to be sent.
-  /// @return messageId The ID of the CCIP message that was sent.
-  function sendMessagePayNative(
-    uint64 _destinationChainSelector,
-    address _receiver,
-    string calldata _text
-  )
-    external
-    onlyOwner
-    onlyAllowlistedDestinationChain(_destinationChainSelector)
-    validateReceiver(_receiver)
-    returns (bytes32 messageId)
-  {
-    // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
-    Client.EVM2AnyMessage memory evm2AnyMessage = _buildCCIPMessage(_receiver, _text, address(0));
-
-    // Initialize a router client instance to interact with cross-chain router
-    IRouterClient router = IRouterClient(this.getRouter());
-
-    // Get the fee required to send the CCIP message
-    uint256 fees = router.getFee(_destinationChainSelector, evm2AnyMessage);
-
-    if (fees > address(this).balance) {
-      revert NotEnoughBalance(address(this).balance, fees);
-    }
-
-    // Send the CCIP message through the router and store the returned CCIP message ID
-    messageId = router.ccipSend{value: fees}(_destinationChainSelector, evm2AnyMessage);
-
-    // Emit an event with message details
-    emit MessageSent(messageId, _destinationChainSelector, _receiver, _text, address(0), fees);
-
-    // Return the CCIP message ID
-    return messageId;
-  }
-
   /// handle a received message
   function _ccipReceive(
     Client.Any2EVMMessage memory any2EvmMessage
   )
     internal
     override
-    onlyAllowlisted(any2EvmMessage.sourceChainSelector, abi.decode(any2EvmMessage.sender, (address))) // Make sure
-    // source chain and sender are allowlisted
 
   {
     s_lastReceivedMessageId = any2EvmMessage.messageId; // fetch the messageId
